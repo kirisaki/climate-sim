79fa6a46debe52d2453f74f09c8bf138ca80fe05
Akihito KIRISAKI <kirisaki@klara.works>
2025-05-17 21:59:04 +0900

    feat: show h3 cells

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 use bevy_pancam::{self, PanCam, PanCamPlugin};
+use geo_types::Coord;
+use h3ron::{H3Cell, Index, ToCoordinate};
+use std::f32::consts::PI;
 
 #[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct HexCoord {
@@ -22,55 +25,91 @@ fn setup(
 ) {
     commands.spawn((Camera2d, PanCam::default()));
 
-    // size of hexmap
-    let width = 10;
-    let height = 8;
-
+    // params for h3
+    let resolution = 5;
+    let center_lat = 35.6895;
+    let center_lon = 139.6917;
+    let center_cell = H3Cell::from_coordinate(
+        Coord {
+            y: center_lat,
+            x: center_lon,
+        },
+        resolution,
+    )
+    .unwrap();
+    let k_distance = match resolution {
+        4 => 7,
+        5 => 20,
+        6 => 30,
+        _ => 15,
+    };
     // params
-    let hex_radius = 30.0;
-    let hex_height = hex_radius * 2.0;
-    let hex_width = hex_radius * 3.0_f32.sqrt();
-    let hex_spacing_x = hex_width;
-    let hex_spacing_y = hex_height * 0.75;
+    let cells = center_cell.grid_disk(k_distance).unwrap();
+    let hex_scaling_factor = match resolution {
+        4 => 800.0,
+        5 => 400.0,
+        6 => 200.0,
+        _ => 400.0,
+    };
+
+    // offset
+    let center_point = h3_to_local_coordinates(&center_cell, hex_scaling_factor);
 
     // generate hexmap
-    for r in 0..height {
-        for q in 0..width {
-            // calcurate offsets
-            let offset_q = q as f32;
-            let offset_r = r as f32;
+    for cell in &cells {
+        // convert from h3
+        let position = h3_to_local_coordinates(&cell, hex_scaling_factor);
 
-            // shift the position if even line
-            let x = offset_q * hex_spacing_x + if r % 2 == 1 { hex_spacing_x / 2.0 } else { 0.0 };
-            let y = -offset_r * hex_spacing_y;
+        // calcurate relatuve position
+        let x = position.0 - center_point.0;
+        let y = position.1 - center_point.1;
 
-            // the color of the hex
-            let color = Color::hsl(
-                ((q as f32 / width as f32) * 360.0) % 360.0,
-                0.5 + (r as f32 / height as f32) * 0.5,
-                0.5,
-            );
+        // calculate hex radius
+        let hex_radius = match resolution {
+            4 => 45.0,
+            5 => 32.5,
+            6 => 15.0,
+            _ => 25.0,
+        };
 
-            // make hex mesh
-            let hex_mesh = meshes.add(RegularPolygon {
-                sides: 6,
-                circumcircle: Circle { radius: hex_radius },
+        // set colors
+        let hue = (Vec2::new(x, y).to_angle() + PI) / (2.0 * PI) * 360.0;
+        let distance_from_center = (x * x + y * y).sqrt() / (hex_scaling_factor * 0.2);
+        let saturation = 1.0 - 0.8 * (distance_from_center / k_distance as f32).min(1.0);
+        let lightness = 0.5;
+        let color = Color::hsl(hue, saturation, lightness);
+
+        // generate hexagonal mesh
+        let hex_mesh = meshes.add(RegularPolygon::new(hex_radius, 6));
+
+        // generate hex
+        commands.spawn((
+            MeshMaterial2d(materials.add(color)),
+            Mesh2d(hex_mesh),
+            Transform::from_xyz(x, y, 0.0).with_rotation(Quat::from_rotation_z(PI / 12.0)),
+        ));
+
+        // display hex infomation
+        let co = cell.to_coordinate().unwrap();
+        let info_text = format!("lat: {:.3}\nlng:{:.3}\nsat: {:.3}", co.y, co.x, saturation);
+        commands.spawn((
+            Text2d::new(info_text),
+            TextFont {
+                font_size: 10.0,
                 ..default()
-            });
-
-            // make hex
-            commands.spawn((
-                Mesh2d(hex_mesh),
-                MeshMaterial2d(materials.add(color)),
-                Transform::from_xyz(x, y, 0.0),
-                HexCoord { q, r },
-            ));
-
-            // add text to the hex
-            commands.spawn((
-                Text2d::new(format!("{},{}", q, r)),
-                Transform::from_xyz(x, y, 1.0),
-            ));
-        }
+            },
+            Transform::from_xyz(x, y, 1.0),
+        ));
     }
+}
+
+fn h3_to_local_coordinates(cell: &H3Cell, scaling_factor: f32) -> (f32, f32) {
+    // get center position of h3 cell
+    let co = cell.to_coordinate().unwrap();
+
+    // convert the coordinate to position
+    let x = co.x as f32 * scaling_factor;
+    let y = co.y as f32 * scaling_factor;
+
+    (x, y)
 }
